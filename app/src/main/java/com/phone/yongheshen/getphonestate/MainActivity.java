3d7f13478de91c83ad9df7b04d966ef676cbe016
shenyonghe525 <906425726@qq.com>
2016-03-23 16:54:46 +0800

    调整
    调整

--- app/src/main/java/com/phone/yongheshen/getphonestate/MainActivity.java
+++ app/src/main/java/com/phone/yongheshen/getphonestate/MainActivity.java
@@ -10,28 +10,31 @@ import android.widget.TextView;
 public class MainActivity extends Activity
 {
 
-    private TextView textView;
+    private TextView textViewInfo;
+    private TextView textViewWH;
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.tv_info);
         getInfo();
+        getWH();
     }
 
     private void getWH(){
         WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
         int width = wm.getDefaultDisplay().getWidth();
         int height = wm.getDefaultDisplay().getHeight();
-        textView.setText("宽度：" + width + "\n高度：" + height);
+        textViewWH = (TextView) findViewById(R.id.tv_wh);
+        textViewWH.setText("宽度：" + width + "\n高度：" + height);
     }
 
     private void getInfo() {
         TelephonyManager mTm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-        String imei = mTm.getDeviceId();//获取运营商sim卡imsi号
-        String imsi = mTm.getSubscriberId();//手机唯一的标识
+        String imei = mTm.getDeviceId();//手机唯一的标识
+        String imsi = mTm.getSubscriberId();//获取运营商sim卡imsi号
         String mtype = android.os.Build.MODEL; // 手机型号
-        textView.setText("sim卡imsi号：" + imei + "\n手机唯一的标识：" + imsi+ "\n手机型号：" + mtype);
+        textViewInfo = (TextView) findViewById(R.id.tv_info);
+        textViewInfo.setText("手机唯一的标识：" + imei + "\nsim卡imsi号：" + imsi+ "\n手机型号：" + mtype);
     }
 }
